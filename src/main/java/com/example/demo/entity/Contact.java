dd832ae6e7c8ca9be9cb242587566194f1985234
Nikitha303 <tcsnew1223@gmail.com>
2022-02-01 15:57:33 +0530

    first commit

---
+++ src/main/java/com/example/demo/entity/Contact.java
@@ -0,0 +1,96 @@
+package com.example.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Contact {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String firstname;
+	private String lastname;
+	private Integer age;
+	private String housenumber;
+	private String street;
+	private String city;
+	private String pin;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="mobile_id", referencedColumnName = "id")
+	Mobile mobile;
+
+	public Mobile getMobile() {
+		return mobile;
+	}
+	public void setMobile(Mobile mobile) {
+		this.mobile = mobile;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getHousenumber() {
+		return housenumber;
+	}
+	public void setHousenumber(String housenumber) {
+		this.housenumber = housenumber;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
